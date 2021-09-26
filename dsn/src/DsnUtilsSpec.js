a04104d251a8c9361b50ddb3c3b2a59ee806e97f
tobiasbrown <2645932+tobiasbrown@users.noreply.github.com>
2021-09-26 14:15:25 +1000

    Refactor tests (#7)
    * Use ES6 imports in test files
    
    * Lint test files
    
    * Replace var with let or const in tests
    
    * Fix incorrect key for longitude and latitude in tests
    
    * Fix incorrect keys used to test station data
    
    * Fix incorrect keys used to test dish data
    
    * Reset signal and target to empty object before parsing data
    
    * Add step to run tests as part of GitHub action
    
    * Replace beforeAll and afterAll with beforeEach and afterEach
    
    * Reset signal to null after each test
    
    * Test correct station name is returned for each dish
    
    * Test warning is logged to console when unknown dish is provided

--- dsn/src/DsnUtilsSpec.js
+++ dsn/src/DsnUtilsSpec.js
@@ -1,14 +1,14 @@
-import DsnUtils from './DsnUtils.js'
+import DsnUtils from './DsnUtils.js';
 
 describe('DsnUtils', function () {
     it('deserializes a domain object identifier', function () {
-        var identifier = DsnUtils.deserializeIdentifier('deep.space.network:canberra');
+        const identifier = DsnUtils.deserializeIdentifier('deep.space.network:canberra');
         expect(identifier.namespace).toBe('deep.space.network');
         expect(identifier.key).toBe('canberra');
     });
 
     it('serializes a domain object identifier', function () {
-        var identifer = DsnUtils.serializeIdentifier({
+        const identifer = DsnUtils.serializeIdentifier({
             namespace: 'deep.space.network',
             key: 'madrid'
         });
@@ -16,15 +16,114 @@ describe('DsnUtils', function () {
         expect(identifer).toBe('deep.space.network:madrid');
     });
 
+    it('logs a warning to the console when unknown dish is provided', function () {
+        spyOn(console, 'warn');
+        DsnUtils.getStationNameByDish('iss');
+        expect(console.warn).toHaveBeenCalled();
+    });
+
+    describe('returns Goldstone station name for', function () {
+        let station;
+
+        afterEach(function () {
+            station = null;
+        });
+
+        it('DSS14', function () {
+            station = DsnUtils.getStationNameByDish('dss14');
+            expect(station).toBe('gdscc');
+        });
+
+        it('DSS24', function () {
+            station = DsnUtils.getStationNameByDish('dss24');
+            expect(station).toBe('gdscc');
+        });
+
+        it('DSS25', function () {
+            station = DsnUtils.getStationNameByDish('dss25');
+            expect(station).toBe('gdscc');
+        });
+
+        it('DSS26', function () {
+            station = DsnUtils.getStationNameByDish('dss26');
+            expect(station).toBe('gdscc');
+        });
+    });
+
+    describe('returns Canberra station name for', function () {
+        let station;
+
+        afterEach(function () {
+            station = null;
+        });
+
+        it('DSS34', function () {
+            station = DsnUtils.getStationNameByDish('dss34');
+            expect(station).toBe('cdscc');
+        });
+
+        it('DSS35', function () {
+            station = DsnUtils.getStationNameByDish('dss35');
+            expect(station).toBe('cdscc');
+        });
+
+        it('DSS36', function () {
+            station = DsnUtils.getStationNameByDish('dss36');
+            expect(station).toBe('cdscc');
+        });
+
+        it('DSS43', function () {
+            station = DsnUtils.getStationNameByDish('dss43');
+            expect(station).toBe('cdscc');
+        });
+    });
+
+    describe('returns Madrid station name for', function () {
+        let station;
+
+        afterEach(function () {
+            station = null;
+        });
+
+        it('DSS54', function () {
+            station = DsnUtils.getStationNameByDish('dss54');
+            expect(station).toBe('mdscc');
+        });
+
+        it('DSS55', function () {
+            station = DsnUtils.getStationNameByDish('dss55');
+            expect(station).toBe('mdscc');
+        });
+
+        it('DSS56', function () {
+            station = DsnUtils.getStationNameByDish('dss56');
+            expect(station).toBe('mdscc');
+        });
+
+        it('DSS63', function () {
+            station = DsnUtils.getStationNameByDish('dss63');
+            expect(station).toBe('mdscc');
+        });
+
+        it('DSS65', function () {
+            station = DsnUtils.getStationNameByDish('dss65');
+            expect(station).toBe('mdscc');
+        });
+    });
+
     describe('parses telemetry', function () {
-        var downSignal;
+        let downSignal;
 
         beforeEach(function () {
             downSignal = document.createElement('downSignal');
         });
 
+        afterEach(function () {
+            downSignal = null;
+        });
+
         it('as a float', function () {
-            var dataRate;
+            let dataRate;
 
             downSignal.setAttribute('dataRate', '160.002853');
             dataRate = DsnUtils.parseTelemetryAsFloatOrString(downSignal, 'dataRate');
@@ -32,9 +131,9 @@ describe('DsnUtils', function () {
         });
 
         it('as a string when a float can not be parsed', function () {
-            var dataRate,
-                frequency,
-                power;
+            let dataRate;
+            let frequency;
+            let power;
 
             downSignal.setAttribute('dataRate', '');
             downSignal.setAttribute('frequency', ' ');
@@ -50,7 +149,7 @@ describe('DsnUtils', function () {
         });
 
         it('as an integer', function () {
-            var dataRate;
+            let dataRate;
 
             downSignal.setAttribute('dataRate', '160');
             dataRate = DsnUtils.parseTelemetryAsIntegerOrString(downSignal, 'dataRate');
@@ -58,9 +157,9 @@ describe('DsnUtils', function () {
         });
 
         it('as a string when an integer can not be parsed', function () {
-            var dataRate,
-                frequency,
-                power;
+            let dataRate;
+            let frequency;
+            let power;
 
             downSignal.setAttribute('dataRate', '');
             downSignal.setAttribute('frequency', ' ');
