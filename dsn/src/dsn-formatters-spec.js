97602707884cdbf4a8dfe93088f6c3c532ba5f7d
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2021-10-24 18:00:15 +1100

    Test wind-speed-to-string formatter functionality

--- dsn/src/dsn-formatters-spec.js
+++ dsn/src/dsn-formatters-spec.js
@@ -1,5 +1,6 @@
 import {
-    angleToString
+    angleToString,
+    windSpeedToString
 } from './dsn-formatters.js';
 
 describe('angle-to-string', function () {
@@ -46,5 +47,52 @@ describe('angle-to-string', function () {
     it('does not validate an undefined angle', function () {
         const canParseAngle = angleToString.validate(undefined);
         expect(canParseAngle).toBeFalse();
+    });
+});
+
+describe('wind-speed-to-string', function () {
+    it('formats the wind speed', function () {
+        const windSpeed = windSpeedToString.format(12);
+        expect(windSpeed).toEqual('12.00 km/hr');
+    });
+
+    it('formats the wind speed to two decimal places', function () {
+        const windSpeed = windSpeedToString.format(12.3456);
+        expect(windSpeed).toEqual('12.35 km/hr');
+    });
+
+    it('formats the wind speed as a string', function () {
+        const windSpeed = windSpeedToString.format('12.3456 kilometres per hour');
+        expect(windSpeed).toEqual('12.3456 kilometres per hour');
+    });
+
+    it('parses the wind speed as a number', function () {
+        const windSpeed = windSpeedToString.parse(12.3456);
+        expect(windSpeed).toEqual(12.3456);
+    });
+
+    it('parses the wind speed as a string', function () {
+        const windSpeed = windSpeedToString.parse('12.3456 km/hr');
+        expect(windSpeed).toEqual(12.3456);
+    });
+
+    it('validates the wind speed as a number', function () {
+        const canParseWindSpeed = windSpeedToString.validate(12.3456);
+        expect(canParseWindSpeed).toBeTrue();
+    });
+
+    it('validates the wind speed as a string', function () {
+        const canParseWindSpeed = windSpeedToString.validate('12.3456 km/hr');
+        expect(canParseWindSpeed).toBeTrue();
+    });
+
+    it('does not validate a null wind speed', function () {
+        const canParseWindSpeed = windSpeedToString.validate(null);
+        expect(canParseWindSpeed).toBeFalse();
+    });
+
+    it('does not validate an undefined wind speed', function () {
+        const canParseWindSpeed = windSpeedToString.validate(undefined);
+        expect(canParseWindSpeed).toBeFalse();
     });
 });
