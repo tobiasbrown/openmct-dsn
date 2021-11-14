0b64ccc51d36587162868f14bc23656385a40f7a
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2021-11-14 19:33:42 +1100

    Test DsnIndicator formats status with multiple active antennas

--- dsn/src/DsnIndicatorSpec.js
+++ dsn/src/DsnIndicatorSpec.js
@@ -25,4 +25,9 @@ describe('DsnIndicator', function () {
         const status = indicator.formatAntennaStatus('Goldstone', ['dss14']);
         expect(status).toEqual('Goldstone: 1 (DSS 14)');
     });
+
+    it('formats status with multiple active antennas', function () {
+        const status = indicator.formatAntennaStatus('Madrid', ['dss54', 'dss55', 'dss63']);
+        expect(status).toEqual('Madrid: 3 (DSS 54, DSS 55, DSS 63)');
+    });
 });
