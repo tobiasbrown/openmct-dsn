63a8eb86b72b9bd256db3f5e1cfe9b33dfc05e26
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2022-07-19 11:56:15 +1000

    Join entry path so webpack can complete build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: path.resolve(__dirname, '/dsn/src/plugin.js'),
+    entry: path.join(__dirname, '/dsn/src/plugin.js'),
     mode: 'development',
     module: {
         rules: [
